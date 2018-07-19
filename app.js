2590f4c245aa4710cf165bdf302fd5c9398f466f
Mehmet Can Taş <mcantas01@gmail.com>
2018-07-19 17:12:04 +0300

    Added code for using static files

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ app.engine('mustache', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mustache');
 
+
+// FOR STATIC FILES (css,js etc.)
+app.use(express.static('public'))
+
+
+
 //ROUTES
 var router = require('./routes/index');
 
